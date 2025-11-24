bdaf0bef2c43d626dc2e2e3eccd4a4050741f186
joaogil78 <joaoreck@yahoo.com.br>
2025-11-23 21:51:19 -0300

    First commit

---
+++ app.js
@@ -0,0 +1,71 @@
+const Amadeus = require('amadeus');
+const express = require('express');
+require('dotenv').config();
+const app = express();
+
+//var http = require('http').createServer(app);
+
+const amadeus = new Amadeus({
+  clientId: process.env.API_KEY,
+  clientSecret: process.env.API_SECRET,
+});
+const port = 3000;
+app.use(express.static('public'));
+
+/* app.get('/', (req, res) => {
+  res.send('Hello World!')
+}); */
+
+// AUTOCOMPLETE
+app.get('/api/autocomplete', async (request, response) => {
+  try {
+    const { query } = request;
+    const { data } = await amadeus.referenceData.locations.get({
+      keyword: query.keyword,
+      subType: Amadeus.location.city,
+    });
+    response.json(data);
+  } catch(error) {
+    console.error(error.response);
+    response.json([]);
+  }
+});
+
+app.get('/api/search', async (request, response) => {
+  try {
+    const { query } = request;
+    const { data } = await amadeus.shopping.flightOffersSearch.get({
+      originLocationCode: query.origin,
+      destinationLocationCode: query.destination,
+      departureDate: query.departureDate,
+      adults: query.adults,
+      children: query.children,
+      infants: query.infants,
+      travelClass: query.travelClass,
+      ...(query.returnDate ? { returnDate: query.returnDate } : {}),
+    });
+    response.json(data);
+
+  } catch(error) {
+    console.error('MY ERROR: ', error);
+    response.json([]);
+  }
+});
+
+/* app.get(`/citysearch`, async (req, res) => {
+  console.log(req.query);
+  var keywords = req.query.keyword;
+  const response = await amadeus.referenceData.locations
+    .get({
+      keyword: keywords,
+      subType: "CITY,AIRPORT",
+    })
+    .catch((x) => console.log(x));
+  try {
+    await res.json(JSON.parse(response.body));
+  } catch (err) {
+    await res.json(err);
+  }
+}); */
+
+app.listen(port, () => console.log('Server start...'));
